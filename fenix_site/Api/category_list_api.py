779731ed62c8e33725c680278782b9dcf7bc1c9a
Sasa537 <sasa5375632@gmail.com>
2022-10-31 15:06:48 +0300

    Renamed some file and add subcategory file

--- fenix_site/Api/category_list_api.py
+++ fenix_site/Api/category_list_api.py
@@ -1,18 +1,23 @@
 from flask import request
-from flask_restful import Resource,Api
+from flask_restful import Resource
 from fenix_site.Models.catalog_model import Catalog, db_catalog
+
 
 class Category(Resource):
     def get(self):
         catalog = Catalog.query.all()
-        return  {"Category":list(x.json() for x in catalog )}
+        return {"Category": list(x.json() for x in catalog)}
+
+    def get(self, id):
+        catalog = Catalog.query.filter_by(id=id).first()
+        return catalog.json()
 
     def post(self):
         data = request.get_json()
         new_category = Catalog(data['name'])
         db_catalog.session.add(new_category)
         db_catalog.session.commit()
-        return new_category.json(),201
+        return new_category.json(), 201
 
     def delete(self, id):
         category = Catalog.query.filter_by(id=id).first()
